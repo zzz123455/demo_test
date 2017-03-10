ba45a07195eed83a27a13258b75ac05348f7453d
zhangmeige <zhangmeige@gridsum.com>
2017-03-10 19:22:09 +0800

    add sum

--- src/demo_test/Dehello.java
+++ src/demo_test/Dehello.java
@@ -4,7 +4,11 @@ public class Dehello {
 
 	public static void main(String[] args) {
 	   
-		System.out.println("ÄãºÃ£¬word!");
+		int a=1;
+		int b=4;
+		int sum;
+		sum=a+b;
+		System.out.println("ÄãºÃ£¬word!"+"  SUM="+sum);
 
 	}
 
